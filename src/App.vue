af9c8734b2ab15777c41d6175b4c96f62ab440c5
rioyuda02 <rio.yudayanto244@gmail.com>
2025-02-14 15:26:50 +0700

    update
    first update

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div id='profiles'>
+    <img class='i-profile' alt="Vue logo" src="@/assets/i-profile.png"/>
+
+    <div class="wrapper">
+      <HelloWorld msg="Hello, I am Rio Yudayanto" />
+
+      <nav >
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About Me</RouterLink>
+      </nav>
+    </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
